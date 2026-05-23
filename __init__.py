31f3addfe0aba8a3fadfa49e71675ef5ca81c089
Rohit Mahto <rohit@champdev.me>
2026-05-23 12:18:23 +0530

    feat: register ChampdevFM node and web directory

--- __init__.py
+++ __init__.py
@@ -1,8 +1,31 @@
-try:
-    from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-except ImportError:
-    NODE_CLASS_MAPPINGS = {}
-    NODE_DISPLAY_NAME_MAPPINGS = {}
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
 
-__version__ = "0.1.0"
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "__version__"]
+try:
+    from .nodes import (
+        NODE_CLASS_MAPPINGS as _SAVE_CLASSES,
+        NODE_DISPLAY_NAME_MAPPINGS as _SAVE_NAMES,
+    )
+    NODE_CLASS_MAPPINGS.update(_SAVE_CLASSES)
+    NODE_DISPLAY_NAME_MAPPINGS.update(_SAVE_NAMES)
+except ImportError:
+    pass
+
+try:
+    from .filemanager import (
+        NODE_CLASS_MAPPINGS as _FM_CLASSES,
+        NODE_DISPLAY_NAME_MAPPINGS as _FM_NAMES,
+    )
+    NODE_CLASS_MAPPINGS.update(_FM_CLASSES)
+    NODE_DISPLAY_NAME_MAPPINGS.update(_FM_NAMES)
+except ImportError:
+    pass
+
+WEB_DIRECTORY = "web"
+__version__ = "0.2.0"
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    "WEB_DIRECTORY",
+    "__version__",
+]
